d8a917af2d850c48eef49685542f2e8af82b81b9
enoumgh <187113084+enoumgh@users.noreply.github.com>
2024-11-10 21:37:55 +0800

    Update weather_report.py
    推送多用户

--- weather_report.py
+++ weather_report.py
@@ -7,8 +7,8 @@ from bs4 import BeautifulSoup
 # 从测试号信息获取
 appID = os.environ.get("APP_ID")
 appSecret = os.environ.get("APP_SECRET")
-# 收信人ID即 用户列表中的微信号
-openId = os.environ.get("OPEN_ID")
+# 收信人ID列表，即多个用户的微信号
+openIds = os.environ.get("OPEN_IDS").split(",")  # 将多个用户ID存入环境变量，逗号分隔
 # 天气预报模板ID
 weather_template_id = os.environ.get("TEMPLATE_ID")
 
@@ -35,61 +35,42 @@ def get_weather(my_city):
                 city_td = tds[-8]
                 this_city = list(city_td.stripped_strings)[0]
                 if this_city == my_city:
-
-                    high_temp_td = tds[-5]
-                    low_temp_td = tds[-2]
-                    weather_type_day_td = tds[-7]
-                    weather_type_night_td = tds[-4]
-                    wind_td_day = tds[-6]
-                    wind_td_day_night = tds[-3]
-
-                    high_temp = list(high_temp_td.stripped_strings)[0]
-                    low_temp = list(low_temp_td.stripped_strings)[0]
-                    weather_typ_day = list(weather_type_day_td.stripped_strings)[0]
-                    weather_type_night = list(weather_type_night_td.stripped_strings)[0]
-
-                    wind_day = list(wind_td_day.stripped_strings)[0] + list(wind_td_day.stripped_strings)[1]
-                    wind_night = list(wind_td_day_night.stripped_strings)[0] + list(wind_td_day_night.stripped_strings)[1]
-
-                    # 如果没有白天的数据就使用夜间的
+                    high_temp = list(tds[-5].stripped_strings)[0]
+                    low_temp = list(tds[-2].stripped_strings)[0]
+                    weather_typ_day = list(tds[-7].stripped_strings)[0]
+                    weather_type_night = list(tds[-4].stripped_strings)[0]
+                    wind_day = "".join(list(tds[-6].stripped_strings))
+                    wind_night = "".join(list(tds[-3].stripped_strings))
+                    
                     temp = f"{low_temp}——{high_temp}摄氏度" if high_temp != "-" else f"{low_temp}摄氏度"
                     weather_typ = weather_typ_day if weather_typ_day != "-" else weather_type_night
-                    wind = f"{wind_day}" if wind_day != "--" else f"{wind_night}"
+                    wind = wind_day if wind_day != "--" else wind_night
+                    
                     return this_city, temp, weather_typ, wind
 
 
 def get_access_token():
-    # 获取access token的url
     url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={}&secret={}' \
         .format(appID.strip(), appSecret.strip())
     response = requests.get(url).json()
-    print(response)
     access_token = response.get('access_token')
     return access_token
 
 
 def get_daily_love():
-    # 每日一句情话
     url = "https://api.lovelive.tools/api/SweetNothings/Serialization/Json"
     r = requests.get(url)
     all_dict = json.loads(r.text)
     sentence = all_dict['returnObj'][0]
-    daily_love = sentence
-    return daily_love
+    return sentence
 
 
 def send_weather(access_token, weather):
-    # touser 就是 openID
-    # template_id 就是模板ID
-    # url 就是点击模板跳转的url
-    # data就按这种格式写，time和text就是之前{{time.DATA}}中的那个time，value就是你要替换DATA的值
-
     import datetime
     today = datetime.date.today()
     today_str = today.strftime("%Y年%m月%d日")
 
-    body = {
-        "touser": openId.strip(),
+    body_template = {
         "template_id": weather_template_id.strip(),
         "url": "https://weixin.qq.com",
         "data": {
@@ -113,20 +94,23 @@ def send_weather(access_token, weather):
             }
         }
     }
-    url = 'https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}'.format(access_token)
-    print(requests.post(url, json.dumps(body)).text)
 
+    # 循环发送消息给每个用户
+    for openId in openIds:
+        body = body_template.copy()
+        body["touser"] = openId.strip()
+        url = f'https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={access_token}'
+        print(requests.post(url, json.dumps(body)).text)
 
 
 def weather_report(this_city):
-    # 1.获取access_token
     access_token = get_access_token()
-    # 2. 获取天气
     weather = get_weather(this_city)
-    print(f"天气信息： {weather}")
-    # 3. 发送消息
-    send_weather(access_token, weather)
-
+    if weather:
+        print(f"天气信息： {weather}")
+        send_weather(access_token, weather)
+    else:
+        print("无法获取天气信息")
 
 
 if __name__ == '__main__':
